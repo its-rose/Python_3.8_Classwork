a296e07beab2b2e3b9552d2df5ff05211cda677b
rosepython <seitakhunova@gmail.com>
2020-11-16 20:28:12 +0600

    classwork

---
+++ Constructions.py
@@ -0,0 +1,69 @@
+# Условные конструкции
+# number1 = int(input('Введите число '))
+# if number1 > 10:
+#     print(f'Вы ввели число {number1}, оно больше 10')
+# else:
+#     print(f'Вы ввели число {number1}, оно меньше 10')
+
+# number1 = int(input('Введите число '))
+# if number1 < 0:
+#     print(f'число {number1} отрицательное')
+# elif number1 > 0:
+#     print(f'число {number1} положительное')
+# else:
+#     print(f'число {number1} равно нулю')
+
+# математические операторы сравнения:
+# < меньше
+# > больше
+# <= меньше или равно
+# >= больше или равно
+# == равняется ли
+# != не равно
+#
+# number1 = int(input('Введите число '))
+# if number1 <= 10:
+#     print(number1)
+# else:
+#     print('число больше 10')
+
+# or -или
+# and
+# not
+# number1 = int(input('Введите число '))
+# if number1 < 10 and number1 > 5:
+#     print(f'число {number1} больше 5 и меньше 10')
+# else:
+#     print('условие не верно')
+
+# if number1 < 10 or number1 == 100:
+#     print(f'число {number1} меньше 10 или равно 100')
+# else:
+#     print('условие не выполнено')
+
+# Калькулятор:
+# operator = input('Введите оператор +, - , /, *:')
+# number1 = int(input('Введите первое число '))
+# number2 = int(input('Введите второе число '))
+# if operator == '+':
+#     print(number1 + number2)
+# elif operator == '-':
+#     print(number1 - number2)
+# elif operator == '/':
+#     print(number1 / number2)
+# elif operator == '*':
+#     print(number1 * number2)
+# else:
+#     print('введен неверный оператор')
+
+# text = 'Введен неверный оператор'
+# if 'оператор' in text:
+#     print('в строке есть слово оператор')
+# else:
+#     print('в строке нет этого слова')
+
+# if 'оператор' not in text:
+#         print('условие истинно')
+# else:
+#         print('условие ложно')
+
