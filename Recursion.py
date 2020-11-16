f1a5373c5b14d31fb9503e01237f75063561bd5a
rosepython <seitakhunova@gmail.com>
2020-11-16 20:34:49 +0600

    classwork

---
+++ Recursion.py
@@ -0,0 +1,12 @@
+# Рекурсия - процесс сам в себе. Если запустить функцию она повторится 997 раз.
+
+# import time
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         time.sleep(1)
+#         print(n)
+#         print(f'{n} * factorial({n} - 1)')
+#         return n * factorial(n - 1)
+# print(factorial(5))
